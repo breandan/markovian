138b8515fff9c99f0c736f39e93d73fe4eb02a44
breandan <bre@ndan.co>
2020-09-17 01:40:00 -0400

    fix sampler and show off distributions

--- README.md
+++ README.md
@@ -2,10 +2,18 @@
 
 Symbolic integration and probabilistic programming in the spirit of [Church](https://web.stanford.edu/~ngoodman/papers/churchUAI08_rev2.pdf) and [Anglican](https://probprog.github.io/anglican/index.html).
 
+# Example
+
+![](exact_pdf.svg)
+
+![](exact_cdf.svg)
+
+![](inversion_sampled_pdf.svg)
+
 # References
 
 * [Symbolic Statistics with SymPy](http://people.cs.uchicago.edu/~mrocklin/tempspace/sympystats.pdf)
 * [Fast and accurate parallel quantile computation](https://discovery.ucl.ac.uk/id/eprint/1482128/1/Luu_thesis.pdf#page=12)
 * [The Harmonic Logarithms and the Binomial Formula](https://core.ac.uk/download/pdf/82415331.pdf)
-* [Closed Form Integration of Articial Neural Networks](https://escholarship.org/content/qt0wz7n7nm/qt0wz7n7nm.pdf#page=5)
-* [A New Distribution on the Simplexwith Auto-Encoding Applications](https://papers.nips.cc/paper/9520-a-new-distribution-on-the-simplex-with-auto-encoding-applications.pdf)
+* [Closed Form Integration of Artificial Neural Networks](https://escholarship.org/content/qt0wz7n7nm/qt0wz7n7nm.pdf#page=5)
+* [A New Distribution on the Simplex with Auto-Encoding Applications](https://papers.nips.cc/paper/9520-a-new-distribution-on-the-simplex-with-auto-encoding-applications.pdf)
--- src/main/kotlin/DSL.kt
+++ src/main/kotlin/DSL.kt
@@ -1,5 +1,6 @@
 import jetbrains.datalore.base.geometry.DoubleVector
 import jetbrains.datalore.plot.PlotSvgExport
+import jetbrains.letsPlot.geom.geom_area
 import jetbrains.letsPlot.geom.geom_density
 import jetbrains.letsPlot.geom.geom_path
 import jetbrains.letsPlot.ggplot
@@ -15,10 +16,13 @@ import java.io.File
 import java.util.*
 import kotlin.math.*
 import kotlin.system.measureTimeMillis
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTimedValue
 
 val util = ExprEvaluator(false, 100)
 
 val rand = kotlin.random.Random(3)
+@ExperimentalTime
 fun main() {
     // TODO: Use regularization to prevent exponents from exploding?
     val a = randomKumaraswamy()
@@ -26,20 +30,28 @@ fun main() {
     val c = randomKumaraswamy()
     val d = randomKumaraswamy()
 
-    val mixture = util.eval("(($a+$b) * ($c+$d))*(($b+$a) * ($c+$d))*(($c+$a) * ($b+$d))").also { println("Mixture: $it") }.also { it.plot2D("PDF") }
-    val integral = util.eval("Integrate($mixture, x)").also { println("CDF: $it") }.also { it.plot2D("CDF") }
-    val norm = util.eval("f(x_):=$integral; f(1.0)").evalDouble().also { println("Norm:$it") }
+//    val mixture = util.eval("$a + $b + $c + $d")
+//    val mixture = util.eval("(($a+$b) * ($c+$d))")
+//    val mixture = util.eval("(($a+$b) * ($c+$d))*(($b+$a) * ($c+$d))*(($c+$a) * ($b+$d))")
+    val mixture = util.eval("1.5*5*x^(1.5-1)*(1-x^1.5)^(5-1) + (5*2*x^(5-1)*(1-x^5)^(2-1))")
+    val mixPlot = mixture.let { println("Mixture: $it"); it.plot2D("Exact PDF") }
+    val integral = measureTimedValue { util.eval("Integrate($mixture, x) + 1") }
+        .let { println("Integration time: ${it.duration}"); it.value }
+    val zero = util.eval("f(x_):=$integral; f(0)").also { println("Zero:$it") }
+    val norm = util.eval("f(x_):=$integral - $zero; f(1)").evalDouble().also { println("Norm:$it") }
+    val cdf = util.eval("($integral - $zero) / $norm")
+    cdf.also { println("CDF: $it"); it.plot2D("Exact CDF") }
     measureTimeMillis {
-        compare({ binarySearch(zero = rand.nextDouble() * norm, exp = integral) })
+        compare({ binarySearch(zero = rand.nextDouble(), exp = cdf) })
     }.also { println("Inversion sampling time: $it ms") }
 
-    measureTimeMillis {
-        compare({
-            val t = Random().nextGaussian()
-            val p = if(rand.nextBoolean()) 1 else -1
-            p * (t + 5) + (1-p) *(t-5)})
-    } // + setOf(-5, -3, -1, 1, 3, 5).random() * 5 }
-        .also { println("Gaussian sampling time: $it ms") }
+//    measureTimeMillis {
+//        compare({
+//            val t = Random().nextGaussian()
+//            val p = if(rand.nextBoolean()) 1 else -1
+//            p * (t + 5) + (1-p) *(t-5)})
+//    } // + setOf(-5, -3, -1, 1, 3, 5).random() * 5 }
+//        .also { println("Gaussian sampling time: $it ms") }
 }
 
 tailrec fun newtonSolver(
@@ -49,8 +61,7 @@ tailrec fun newtonSolver(
     exp_dx: IExpr = util.eval("D($exp, x)"),
     nextGuess: Double = guess - util.eval("f(x_):=($exp)/($exp_dx); f($guess)").evalDouble(),
 ): Double =
-    if (abs(util.eval("f(x_):=$exp - $zero; f($guess)").evalDouble()) < 0.001)
-        guess.also { println("$zero/$it: ${util.eval("f(x_):=$exp; f($guess)").evalDouble()}") }
+    if (abs(util.eval("f(x_):=$exp - $zero; f($guess)").evalDouble()) < 0.001) guess
     else newtonSolver(zero = zero, guess = nextGuess, exp = exp, exp_dx = exp_dx)
 
 // Only works on monotonically increasing functions (e.g. CDF)
@@ -63,7 +74,7 @@ tailrec fun binarySearch(
     delta: Double = 0.5.pow(iter) * (range.endInclusive - range.start).absoluteValue,
     eval: Double = util.eval("f(x_):=$exp; f($guess)").evalDouble(),
     error: Double = zero - eval
-): Double = if (error.absoluteValue < 0.01 || iter > 20) guess
+): Double = if (error.absoluteValue < 0.01 || iter > 200) guess
 else if (error < 0) binarySearch(iter = iter + 1, guess = guess - delta, exp = exp, zero = zero)
 else binarySearch(iter = iter + 1, guess = guess + delta, exp = exp, zero = zero)
 
@@ -72,12 +83,10 @@ private fun IExpr.plot2D(title: String, norm: Double = 1.0) {
     val xs = (0.01..1.0 step 0.01).toList()
     val ys = listOf(xs.map { util.eval("f(x_):=$this; f($it)").evalDouble() / norm })
     val data = (labels.zip(ys) + ("x" to xs)).toMap()
-    val colors = listOf("dark_green", "gray", "black", "red", "orange", "dark_blue")
-    val geoms = labels.zip(colors).map { geom_path(size = 2.0, color = it.second) { x = "x"; y = "y" } }
+    val geoms = labels.map { geom_area(size = 2.0, color = "dark_green", fill = "light_green") { x = "x"; y = "y" } }
     val plot = geoms.foldRight(ggplot(data)) { it, acc -> acc + it } + ggtitle(title)
     plot.display()
 }
-
 
 infix fun ClosedRange<Double>.step(step: Double): Iterable<Double> {
     require(start.isFinite())
@@ -94,27 +103,31 @@ infix fun ClosedRange<Double>.step(step: Double): Iterable<Double> {
 enum class Domain { INT, RATIONAL, DOUBLE }
 
 // https://en.wikipedia.org/wiki/Kumaraswamy_distribution
-fun randomKumaraswamy(v: String = "x", domain: Domain = Domain.INT) =
+fun randomKumaraswamy(v: String = "x", domain: Domain = Domain.RATIONAL) =
     when(domain) {
-        Domain.INT -> "${rand.nextInt(1, 5)}".let{
-            it to "1"
+        Domain.INT -> "${rand.nextInt(2, 10)}".let {
+            it to "${rand.nextInt(2, 10)}"
 //            if (rand.nextBoolean()) "1" to it else it to "1"
         }//"${rand.nextInt(1, 5)}"
         Domain.RATIONAL -> rand.nextInt(2, 10).let { a -> "$a/${rand.nextInt(1, a)}" }.let {
 //            it to "1"
             if (rand.nextBoolean()) "1" to it else it to "1"
+//            it to rand.nextInt(2, 10).let { a -> "$a/${rand.nextInt(1, a)}" }
         }
-        Domain.DOUBLE -> "${rand.nextDouble() * 5.0 + 1}" to "${rand.nextDouble() * 5.0 + 1}"
+        Domain.DOUBLE -> "${rand.nextDouble() * 5.0 + 1}".take(3) to "${rand.nextDouble() * 5.0 + 1}".take(3)
     }.let { (a, b) ->
         "($a*$b*$v^($a-1)*(1-$v^$a)^($b-1))"
     }
 
 // https://escholarship.org/content/qt0wz7n7nm/qt0wz7n7nm.pdf#page=5
 fun randomGottschling(v: String = "x") =
-    rand.nextDouble().let { l ->
-        val g1 = Gamma.gamma((l + 1)/l)
-        val g2 = Gamma.gamma(1/(2*l))
-        "${(g1/g2)* sqrt(l / PI)}($l*$v^2 + 1)^(${-0.5*(1.0+1.0/l)})"
+//    rand.nextInt(2, 10).let { a -> "$a/${rand.nextInt(1, a)}" }
+    rand.nextDouble()
+    .let { l ->
+//        val g1 = Gamma.gamma((l + 1)/l)
+//        val g2 = Gamma.gamma(1/(2*l))
+//        "${(g1/g2)* sqrt(l / PI)}($l*$v^2 + 1)^(${-0.5*(1.0+1.0/l)})"
+        "(Gamma(($l + 1)/$l)/Gamma(1/(2*$l))) * sqrt($l / PI)($l*$v^2 + 1)^(-(1/2)*(1+1/$l))"
     }
 
 fun randomLogistic(v: String = "x") =
@@ -140,7 +153,7 @@ fun randomSigmoid(v: String = "x") = (rand.nextDouble()).let { i ->
     "ln(1 + E^($v))"
 }
 
-val POPCOUNT = 1000
+val POPCOUNT = 10000
 
 fun compare(vararg samplers: () -> Double) =
     compare(*samplers.map { f -> List(POPCOUNT) { f() } }.toTypedArray())
