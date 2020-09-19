eda121f64c30aac1ce9df9bdf6f0a9829734b13b
breandan <bre@ndan.co>
2020-09-19 19:31:29 -0400

    poisson, random.nextRational() and coroutines

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,12 +7,16 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://jitpack.io")
     maven("https://oss.sonatype.org/content/repositories/snapshots")
     maven("https://jetbrains.bintray.com/lets-plot-maven")
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
+
+//    implementation("com.github.axkr:symja_kotlin:-0164fc62ff-1")
     implementation("org.matheclipse:matheclipse-core:1.0.0-SNAPSHOT")
     implementation("org.jetbrains.lets-plot-kotlin:lets-plot-kotlin-api:1.0.0")
 }
--- src/main/kotlin/DSL.kt
+++ src/main/kotlin/DSL.kt
@@ -2,53 +2,59 @@ import jetbrains.datalore.base.geometry.DoubleVector
 import jetbrains.datalore.plot.PlotSvgExport
 import jetbrains.letsPlot.geom.geom_area
 import jetbrains.letsPlot.geom.geom_density
-import jetbrains.letsPlot.geom.geom_path
 import jetbrains.letsPlot.ggplot
 import jetbrains.letsPlot.ggsize
 import jetbrains.letsPlot.intern.Plot
 import jetbrains.letsPlot.intern.toSpec
 import jetbrains.letsPlot.label.ggtitle
 import jetbrains.letsPlot.lets_plot
-import org.hipparchus.special.Gamma
+import kotlinx.coroutines.*
 import org.matheclipse.core.eval.ExprEvaluator
 import org.matheclipse.core.interfaces.IExpr
 import java.io.File
-import java.util.*
-import kotlin.math.*
+import kotlin.math.abs
+import kotlin.math.absoluteValue
+import kotlin.math.pow
+import kotlin.random.Random
 import kotlin.system.measureTimeMillis
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTimedValue
 
 val util = ExprEvaluator(false, 100)
 
-val rand = kotlin.random.Random(3)
 @ExperimentalTime
-fun main() {
+fun main(): Unit = with(Random(3)) {
     // TODO: Use regularization to prevent exponents from exploding?
-    val a = randomKumaraswamy()
-    val b = randomKumaraswamy()
-    val c = randomKumaraswamy()
-    val d = randomKumaraswamy()
+    val a = nextKumaraswamy()
+    val b = nextKumaraswamy()
+    val c = nextKumaraswamy()
+    val d = nextKumaraswamy()
 
-//    val mixture = util.eval("$a + $b + $c + $d")
-    val mixture = util.eval("(($a+$b) * ($c+$d))")
+    val mixture = util.eval("$a + $b + $c + $d")
+//    val mixture = util.eval("(($a+$b) * ($c+$d))")
 //    val mixture = util.eval("(($a+$b) * ($c+$d))*(($b+$a) * ($c+$d))*(($c+$a) * ($b+$d))")
 //    val mixture = util.eval("1.5*5*x^(1.5-1)*(1-x^1.5)^(5-1) + (5*2*x^(5-1)*(1-x^5)^(2-1))")
     val mixPlot = mixture.let { println("Mixture: $it"); it.plot2D("Exact PDF") }
-    val integral = measureTimedValue { util.eval("Integrate($mixture, x) + 1") }
+    val integral = measureTimedValue { util.eval("Integrate($mixture, x)") }
         .let { println("Integration time: ${it.duration}"); it.value }
     val zero = util.eval("f(x_):=$integral; f(0)").also { println("Zero:$it") }
     val norm = util.eval("f(x_):=$integral - $zero; f(1)").evalDouble().also { println("Norm:$it") }
     val cdf = util.eval("($integral - $zero) / $norm")
-    cdf.also { println("CDF: $it"); it.plot2D("Exact CDF") }
-    measureTimeMillis {
-        compare({ binarySearch(zero = rand.nextDouble(), exp = cdf) })
-    }.also { println("Inversion sampling time: $it ms") }
+//    cdf.also { println("CDF: $it"); it.plot2D("Exact CDF") }
+    measureTimeMillis { compare({ cdf.sample(this) }) }
+        .also { println("Inversion sampling time: $it ms") }
 }
+
+fun IExpr.sample(random: Random): Double =
+    try {
+        binarySearch(zero = random.nextRational(), exp = this)
+    } catch (e: Exception) {
+        sample(random)
+    }
 
 tailrec fun newtonSolver(
     exp: IExpr,
-    zero: Double = 0.0,
+    zero: String,
     guess: Double = binarySearch(exp = exp, zero = zero),
     exp_dx: IExpr = util.eval("D($exp, x)"),
     nextGuess: Double = guess - util.eval("f(x_):=($exp)/($exp_dx); f($guess)").evalDouble(),
@@ -59,13 +65,13 @@ tailrec fun newtonSolver(
 // Only works on monotonically increasing functions (e.g. CDF)
 tailrec fun binarySearch(
     exp: IExpr,
-    zero: Double,
+    zero: String,
     iter: Int = 1,
     range: ClosedFloatingPointRange<Double> = 0.0..1.0,
     guess: Double = (range.endInclusive - range.start) / 2.0,
     delta: Double = 0.5.pow(iter) * (range.endInclusive - range.start).absoluteValue,
-    eval: Double = util.eval("f(x_):=$exp; f($guess)").evalDouble(),
-    error: Double = zero - eval
+    eval: IExpr= util.eval("f(x_):=$exp; f($guess)"),
+    error: Double = util.eval("$zero - $eval").evalDouble()
 ): Double = if (error.absoluteValue < 0.01 || iter > 200) guess
 else if (error < 0) binarySearch(iter = iter + 1, guess = guess - delta, exp = exp, zero = zero)
 else binarySearch(iter = iter + 1, guess = guess + delta, exp = exp, zero = zero)
@@ -73,7 +79,7 @@ else binarySearch(iter = iter + 1, guess = guess + delta, exp = exp, zero = zero
 private fun IExpr.plot2D(title: String, norm: Double = 1.0) {
     val labels = arrayOf("density")
     val xs = (0.01..1.0 step 0.01).toList()
-    val ys = listOf(xs.map { util.eval("f(x_):=$this; f($it)").evalDouble() / norm })
+    val ys = listOf(xs.pmap { util.eval("f(x_):=$this; f($it)").evalDouble() / norm })
     val data = (labels.zip(ys) + ("x" to xs)).toMap()
     val geoms = labels.map { geom_area(size = 2.0, color = "dark_green", fill = "light_green") { x = "x"; y = "density" } }
     val plot = geoms.foldRight(ggplot(data)) { it, acc -> acc + it } + ggtitle(title)
@@ -96,61 +102,72 @@ enum class Domain { INT, RATIONAL, DOUBLE }
 //var switch = false
 
 // https://en.wikipedia.org/wiki/Kumaraswamy_distribution
-fun randomKumaraswamy(v: String = "x", domain: Domain = Domain.RATIONAL) =
+fun Random.nextKumaraswamy(v: String = "x", domain: Domain = Domain.RATIONAL) =
     when(domain) {
-        Domain.INT -> "${rand.nextInt(2, 10)}".let {
-//            it to "${rand.nextInt(2, 10)}"
-            if (rand.nextBoolean()) "1" to it else it to "1"
-        }//"${rand.nextInt(1, 5)}"
-        Domain.RATIONAL -> rand.nextInt(2, 50).let { a -> "$a/${rand.nextInt(1, a)}" }.let {
+        Domain.INT -> "${nextInt(2, 10)}".let {
+//            it to "${nextInt(2, 10)}"
+            if (nextBoolean()) "1" to it else it to "1"
+        }//"${nextInt(1, 5)}"
+        Domain.RATIONAL -> nextRational().let {
 //            it to "1"
 //            switch = !switch
-            if (rand.nextBoolean()) "1" to it else it to "1"
-//            it to rand.nextInt(2, 50).let { a -> "$a/${rand.nextInt(1, a)}" }
+            if (nextBoolean()) "1" to it else it to "1"
+//            it to nextInt(2, 50).let { a -> "$a/${nextInt(1, a)}" }
         }
-        Domain.DOUBLE -> "${rand.nextDouble() * 5.0 + 1}".take(3) to "${rand.nextDouble() * 5.0 + 1}".take(3)
+        Domain.DOUBLE -> "${nextDouble() * 5.0 + 1}".take(3) to "${nextDouble() * 5.0 + 1}".take(3)
     }.let { (a, b) ->
         "($a*$b*$v^($a-1)*(1-$v^$a)^($b-1))"
     }
 
 // https://escholarship.org/content/qt0wz7n7nm/qt0wz7n7nm.pdf#page=5
-fun randomGottschling(v: String = "x") =
-//    rand.nextInt(2, 10).let { a -> "$a/${rand.nextInt(1, a)}" }
-    rand.nextDouble()
-    .let { l ->
+fun Random.nextGottschling(v: String = "x") =
+    // mRational()
+    nextDouble().let { l ->
 //        val g1 = Gamma.gamma((l + 1)/l)
 //        val g2 = Gamma.gamma(1/(2*l))
 //        "${(g1/g2)* sqrt(l / PI)}($l*$v^2 + 1)^(${-0.5*(1.0+1.0/l)})"
         "(Gamma(($l + 1)/$l)/Gamma(1/(2*$l))) * sqrt($l / PI)($l*$v^2 + 1)^(-(1/2)*(1+1/$l))"
     }
 
-fun randomLogistic(v: String = "x") =
-    (rand.nextInt(1, 10) to rand.nextInt(1, 2)).let { (u, s) ->
-        "(1/(4*$s))*sech(($v-$u)/(2*$s))^2"
-    }
+fun Random.nextLogistic(v: String = "x") =
+    (nextInt(1, 10) to nextInt(1, 2)).let { (u, s) -> "(1/(4*$s))*sech(($v-$u)/(2*$s))^2" }
 
 // https://core.ac.uk/download/pdf/82415331.pdf
-fun randomHarmonic(v: String = "x") =
-    (rand.nextInt(1, 10) to rand.nextInt(1, 10)).let { (i, j) ->
-        "$v^$i * log($v)^$j"
-    }
+fun Random.nextHarmonic(v: String = "x") =
+    (nextInt(1, 10) to nextInt(1, 10)).let { (i, j) -> "$v^$i * log($v)^$j" }
 
-fun randomExpontential(v: String = "x") = (rand.nextInt(0, 10) to rand.nextInt(0, 10)).let { (i, j) ->
-    "$i * E^($v-$j)"
+fun Random.nextExpontential(v: String = "x") =
+    (nextInt(0, 10) to nextInt(0, 10)).let { (i, j) -> "$i * E^($v-$j)" }
+
+fun Random.nextPolynomial(v: String = "x") =
+    (nextDouble() to nextInt(1, 3)).let { (i, j) -> "$i * $v^$j" }
+
+fun Random.nextSigmoid(v: String = "x") =
+    nextDouble().let { i -> "ln(1 + E^($v))" }
+
+// https://www.researchgate.net/profile/Ekaterina_Karatsuba/publication/246166981_Fast_evaluation_of_transcendental_functions/links/0deec528ab5b45f8bc000000/Fast-evaluation-of-transcendental-functions.pdf
+fun Random.nextPoisson(v: String = "x") =
+    nextRational().let { λ -> "E^(-$λ) * $λ^$v / $v!" }
+
+// Generates a rational number in (0, 1). Approximates nextDouble() as n -> \infty
+tailrec fun Random.nextRational(n: Int = 100): String {
+    val (j, i) = nextInt(3, n).let { it to nextInt(1, it - 1) }
+    return if (1 < i.gcd(j)) nextRational(n) else "$i/$j"
 }
 
-fun randomPolynomial(v: String = "x") = (rand.nextDouble() to rand.nextInt(1, 3)).let { (i, j) ->
-    "$i * $v^$j"
-}
-
-fun randomSigmoid(v: String = "x") = (rand.nextDouble()).let { i ->
-    "ln(1 + E^($v))"
-}
+tailrec fun Int.gcd(b: Int): Int =
+    if (this == b) this
+    else if (this > b) (this - b).gcd(b)
+    else gcd(b - this)
 
 val POPCOUNT = 10000
 
-fun compare(vararg samplers: () -> Double) =
-    compare(*samplers.map { f -> List(POPCOUNT) { f() } }.toTypedArray())
+fun <A, B> Iterable<A>.pmap(f: suspend (A) -> B): List<B> = runBlocking {
+    map { async { f(it) } }.awaitAll()
+}
+
+fun compare(vararg samplers: () -> Double): Unit =
+    compare(*samplers.map { f -> (0..POPCOUNT).pmap { f() } }.toTypedArray())
 
 fun compare(vararg samples: List<Double>) {
     val data = mapOf<String, Any>(
@@ -164,7 +181,6 @@ fun compare(vararg samples: List<Double>) {
     p + ggsize(2000, 1000)
     p.display()
 }
-
 
 fun Plot.display() =
     File.createTempFile("test", ".svg").also {
