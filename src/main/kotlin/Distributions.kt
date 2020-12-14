c6b9e616600808d9bd39e542f9333a620ad6e5fb
breandan <bre@ndan.co>
2020-12-14 04:50:56 -0500

    generify distribution

--- src/main/kotlin/Distributions.kt
+++ src/main/kotlin/Distributions.kt
@@ -2,7 +2,6 @@
 
 import umontreal.ssj.probdist.*
 import kotlin.math.pow
-import kotlin.reflect.KProperty
 
 class Uniform(
   override val name: String = "",
@@ -10,7 +9,7 @@ class Uniform(
   val hi: Double = 1.0,
   override val μ: Double = (hi - lo) / 2.0,
   override val σ: Double = (hi - lo).pow(2) / 12.0
-) : Distribution<Uniform>() {
+): Dist<Uniform> {
   override val density: ContinuousDistribution = UniformDist(lo, hi)
   override fun new(name: String): Uniform = Uniform(name, lo, hi)
 }
@@ -21,66 +20,30 @@ class Beta(
   val β: Double = 2.0,
   override val μ: Double = α / (α + β),
   override val σ: Double = α * β / ((α + β).pow(2) * (α + β + 1))
-) : Distribution<Beta>() {
+): Dist<Beta> {
   override val density: ContinuousDistribution = BetaDist(α, β)
   override fun new(name: String): Beta = Beta(name, α, β)
 }
 
-abstract class Distribution<T: Distribution<T>> : (Double) -> Double {
-  open val name: String = ""
-  abstract val μ: Double
-  abstract val σ: Double
-
-  open operator fun getValue(nothing: Nothing?, property: KProperty<*>): T =
-    new(property.name)
-  open val density: ContinuousDistribution = UniformDist()
-
-  abstract fun new(name: String): T
-  override fun invoke(p1: Double): Double = density.inverseF(p1)
-  open fun pdf(x: Double) = density.density(x)
-
-//    abstract operator fun plus(distribution: T): T
-//    abstract operator fun times(distribution: T): T
-
-  /**
-   * When observing new data, we should:
-   *
-   *  1. Recurse and bind on a match.
-   *  2. Update our prior belief.
-   *  3. Propagate uncertainty forward.
-   */
-
-  fun observe(vararg pairs: Pair<T, List<Double>>): T = TODO()
-
-  // TODO: Combinators: average, convolution, product, sum...
-
-  tailrec fun cdf(
-    z: Double,
-    sum: Double = 0.0,
-    term: Double = z,
-    i: Int = 3
-  ): Double =
-    when {
-      z < -8.0 -> 0.0
-      z > 8.0 -> 1.0
-      sum + term == sum -> 0.5 + sum * pdf(z)
-      else -> cdf(z, sum + term, term * z * z / i, i + 2)
-    }
-
-  // Binary search root-finder
-  tailrec fun invcdf(
-    y: Double, lo: Double = -4.0, hi: Double = 8.0,
-    mid: Double = lo + (hi - lo) / 2
-  ): Double = when {
-    hi - lo < precision -> mid
-    cdf(mid) < y -> invcdf(y, mid, hi)
-    else -> invcdf(y, lo, mid)
+tailrec fun <D: Dist<D>> Dist<D>.cdf(
+  z: Double,
+  sum: Double = 0.0,
+  term: Double = z,
+  i: Int = 3
+): Double =
+  when {
+    z < -8.0 -> 0.0
+    z > 8.0 -> 1.0
+    sum + term == sum -> 0.5 + sum * pdf(z)
+    else -> cdf(z, sum + term, term * z * z / i, i + 2)
   }
 
-  override fun equals(other: Any?) =
-    other is Distribution<*> &&
-      javaClass == other.javaClass &&
-      μ == other.μ && σ == other.σ
-
-  override fun toString() = "${javaClass.simpleName}($μ, $σ)"
+// Binary search root-finder
+tailrec fun <D: Dist<D>> Dist<D>.invcdf(
+  y: Double, lo: Double = -4.0, hi: Double = 8.0,
+  mid: Double = lo + (hi - lo) / 2
+): Double = when {
+  hi - lo < precision -> mid
+  cdf(mid) < y -> invcdf(y, mid, hi)
+  else -> invcdf(y, lo, mid)
 }
--- src/main/kotlin/Mixtures.kt
+++ src/main/kotlin/Mixtures.kt
@@ -2,6 +2,7 @@
 
 import umontreal.ssj.probdist.*
 import kotlin.math.*
+import kotlin.reflect.KProperty
 
 // TODO: https://en.wikipedia.org/wiki/Plate_notation
 // cf. https://github.com/todesking/platebuilder
@@ -10,25 +11,62 @@ import kotlin.math.*
 // https://en.wikipedia.org/wiki/Exponential_family#Table_of_distributions
 // https://en.wikipedia.org/wiki/Cumulant
 // http://indico.ictp.it/event/a0143/contribution/2/material/0/0.pdf
+// https://en.wikipedia.org/wiki/Stable_distribution
+// https://en.wikipedia.org/wiki/Relationships_among_probability_distributions
+
+// https://en.wikipedia.org/wiki/Algebra_of_random_variables
+class RandomVariable(val distribution: Dist<*>): (Double) -> Double {
+// https://en.wikipedia.org/wiki/Sum_of_normally_distributed_random_variables
+// https://en.wikipedia.org/wiki/List_of_convolutions_of_probability_distributions
+// https://stats.stackexchange.com/questions/21549/how-to-add-two-dependent-random-variables
+// https://math.stackexchange.com/questions/1402520/pdf-of-sum-of-two-dependent-random-variables
+  operator fun plus(that: RandomVariable): RandomVariable =
+    RandomVariable(distribution + that.distribution)
+
+// https://en.wikipedia.org/wiki/Product_distribution
+// http://www.math.wm.edu/~leemis/2003csada.pdf
+  operator fun times(that: RandomVariable): RandomVariable =
+    RandomVariable(distribution * that.distribution)
+
+  infix fun given(that: RandomVariable): Nothing = TODO()
+
+  override fun invoke(p1: Double) = distribution(p1)
+
+// https://en.wikipedia.org/wiki/Ratio_distribution
+//  operator fun div(that: RandomVariable): RandomVariable = TODO()
+}
 
 class Gaussian(
   override val name: String = "",
   override val μ: Double = 0.1,
   override val σ: Double = 1.0
-): GaussianMixture(name, listOf()) {
+): Dist<Gaussian> {
   override val density: ContinuousDistribution = NormalDist(μ, σ)
   override fun new(name: String): Gaussian = Gaussian(name, μ, σ)
-  override val components by lazy { listOf(this) }
 
   // TODO: can we get the graph compiler to infer this?
   // https://math.stackexchange.com/questions/1112866/product-of-two-gaussian-pdfs-is-a-gaussian-pdf-but-product-of-two-gaussian-vari
-  override operator fun times(that: Gaussian) =
+  operator fun times(that: Gaussian) =
     combine(this, that) { μ1, μ2, σ1, σ2 ->
       Gaussian(
         "$name * ${that.name}",
         μ = (μ1 * σ2 * σ2 + μ2 * σ1 * σ1) / (σ1 * σ1 + σ2 * σ2),
         σ = (σ1 * σ2).pow(2) / (σ1 * σ1 + σ2 * σ2)
       )
+    }
+
+  override fun times(that: Dist<*>): Dist<*> =
+    when(that) {
+      is Gaussian -> this * that
+      is Mixture<*> -> that * this
+      else -> TODO()
+    }
+
+  override fun plus(that: Dist<*>): Dist<*> =
+    when(that) {
+      is Gaussian -> Mixture<Gaussian>(this) + that
+      is Mixture<*> -> that as Mixture<*> + this
+      else -> TODO()
     }
 
   companion object {
@@ -42,12 +80,42 @@ class Gaussian(
   operator fun plus(c: Double) = Gaussian(μ = c + μ, σ = σ)
 }
 
+interface Dist<D: Dist<D>> : (Double) -> Double {
+  open val name: String
+  abstract val μ: Double
+  abstract val σ: Double
+
+  open operator fun getValue(nothing: Nothing?, property: KProperty<*>): D =
+    new(property.name)
+  open val density: ContinuousDistribution
+
+  abstract fun new(name: String): D
+  override fun invoke(p1: Double): Double = density.inverseF(p1)
+  fun sample() = RandomVariable(this)
+  open fun pdf(x: Double) = density.density(x)
+
+  operator fun plus(that: Dist<*>): Dist<*> = TODO()
+  operator fun times(that: Dist<*>): Dist<*> = TODO()
+  infix fun convolve(d: Dist<*>): Dist<*> = TODO()
+
+  /**
+   * When observing new data, we should:
+   *
+   *  1. Recurse and bind on a match.
+   *  2. Update our prior belief.
+   *  3. Propagate uncertainty forward.
+   */
+
+  fun observe(vararg pairs: Pair<D, List<Double>>): D = TODO()
+}
+
 // https://arxiv.org/pdf/1901.06708.pdf
 
-open class GaussianMixture(
+open class Mixture<T: Dist<T>>(
   override val name: String = "",
-  open val components: List<GaussianMixture>
-): Distribution<GaussianMixture>() {
+  open val components: List<Dist<*>>
+): Dist<Mixture<T>> {
+  constructor(vararg ds: Dist<*>): this(components = ds.toList())
   // Uniform by default
   val weights: List<Double> by lazy {
     List(components.size) { 1.0 / components.size.toDouble() }
@@ -64,19 +132,47 @@ open class GaussianMixture(
       override fun density(x: Double) = partition { it.density.density(x) }
     }
 
-  operator fun plus(that: Gaussian) =
-    GaussianMixture(components = components + listOf(that))
+  class Sum<T: Dist<T>>(
+    override val name: String = "",
+    override val components: List<Dist<*>>
+  ): Mixture<T>(name, components)
 
-  open operator fun times(that: Gaussian): GaussianMixture =
-    GaussianMixture(components = components.map { it * that })
+  class Prod<T: Dist<T>>(
+    override val name: String = "",
+    override val components: List<Dist<*>>
+  ): Mixture<T>(name, components)
 
-  open operator fun times(that: GaussianMixture): GaussianMixture =
-    GaussianMixture(components = components.map { it * that })
+  @JvmName("mixPlus")
+  operator fun plus(that: T): Mixture<T> =
+    Sum<T>(components = components + that)
 
-  fun partition(selector: (GaussianMixture) -> Double) =
+  operator fun plus(that: Mixture<T>): Mixture<T> =
+    Mixture<T>(components = components + that.components)
+
+  open operator fun times(that: T): Mixture<T> =
+    Mixture<T>(components = components.map { it * that })
+
+  operator fun times(that: Mixture<T>): Mixture<T> =
+    Mixture<T>(components = components.map { it })
+
+  override fun times(that: Dist<*>): Dist<*> =
+    when(that) {
+      is Gaussian -> this * that as T
+      is Mixture<*> -> this * that as Mixture<T>
+      else -> TODO()
+    }
+
+  override fun plus(that: Dist<*>): Dist<*> =
+    when(that) {
+      is Gaussian -> this + that as T
+      is Mixture<*> -> this + that as Mixture<T>
+      else -> TODO()
+    }
+
+  fun partition(selector: (Dist<*>) -> Double) =
     components.map { selector(it) }.zip(weights).sumByDouble { (a, b) -> a * b }
 
-  override fun new(name: String) = GaussianMixture(name, components)
+  override fun new(name: String) = Mixture<T>(name, components)
 }
 
 const val precision = 0.00000001
@@ -90,8 +186,26 @@ fun main() {
 
   val g4 = Gaussian("", 5.0, 2.0)
 
-  val g5 = g3 * g4
+  val g7 = Gaussian("", 5.0, 2.0)
+  val g5 = g3 * g4 + g7
 
   // TODO: test distributivity holds
   compare(g0, g1, g2, g3, g4, g5).display()
 }
+
+//  P { g1 < g2 }.given()
+//  u <- 1
+//  val t: Boolean = true
+//  val  = 1
+//
+//  if(g1 <- g2) {
+//    println("test")
+//  }
+//}
+//
+//fun P(b: Ctx.() -> Boolean): (Int) ->  Boolean {
+//  b(Ctx())
+//}
+//class Ctx {
+//  fun given()
+//}
