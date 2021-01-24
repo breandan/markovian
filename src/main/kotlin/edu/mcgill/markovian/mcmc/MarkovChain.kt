d5b74524c39b18132720fddb9948b5e3a753e726
breandan <bre@ndan.co>
2021-01-23 23:10:37 -1100

    speed up sampler

--- src/main/kotlin/edu/mcgill/markovian/mcmc/MarkovChain.kt
+++ src/main/kotlin/edu/mcgill/markovian/mcmc/MarkovChain.kt
@@ -5,19 +5,26 @@ import org.jetbrains.kotlinx.multik.api.*
 import org.jetbrains.kotlinx.multik.ndarray.data.*
 import org.jetbrains.kotlinx.multik.ndarray.operations.*
 import kotlin.random.Random
+import kotlin.streams.asStream
+import kotlin.system.measureTimeMillis
 
 fun main() {
   val data =
     "lorem ipsum dolor sit amet, consectetur adipiscing elit. aliquam tempus nisi eu nisl gravida, in pretium tellus cursus. duis facilisis malesuada ligula et interdum. donec ac libero et dui tempus bibendum. donec porttitor mollis accumsan. sed urna turpis, consectetur sit amet gravida vitae, pellentesque in libero. quisque erat lorem, tincidunt eu vestibulum eu, dapibus nec felis. ut eu purus tortor. nulla eros leo, porttitor vel elit eget, tempor blandit metus. proin congue lobortis pretium. nulla eget pellentesque risus. nam ultrices quis tellus ut tincidunt. morbi vestibulum ipsum eu elementum scelerisque. fusce aliquam lobortis urna vel rhoncus. duis ut rhoncus purus, id auctor odio. donec lobortis ac enim in placerat. donec placerat nec lectus a bibendum.".asSequence()
-  val mc = data.toMarkovChain()
-  val sample = mc.sample().take(100).toList()
-  println("Sample: " + sample.joinToString(""))
-  println("Ergodic: " + mc.isErgodic())
+
+  measureTimeMillis {
+    val mc = data.toMarkovChain()
+    measureTimeMillis {
+      val sample = mc.sample().take(100).toList()
+      println("Sample: " + sample.joinToString(""))
+      println("Ergodic: " + mc.isErgodic())
+    }.also { println("Sampling time: $it ms") }
+  }.also { println("Total time: $it ms") }
 }
 
 fun <T> Sequence<T>.toMarkovChain() =
   MarkovChain<T>().also { mc ->
-    reduce { prev, curr ->
+    asStream().parallel().reduce { prev, curr ->
       mc.observe(prev to curr)
       curr
     }
@@ -26,21 +33,22 @@ fun <T> Sequence<T>.toMarkovChain() =
 class MarkovChain<T> {
   val keys
     get() = counts.asMap().keys
-      .flatMap { listOf(it.first, it.second) }
+      .flatMap { listOf(it.first, it.second) }.distinct()
   val size get() = keys.size
   val counts = AtomicLongMap.create<Pair<T, T>>()
 
   fun sample() =
-    transitionMatrix().let { tm ->
-      generateSequence(
-        seedFunction = {
-          this[mk.math.sumD2(tm, 1).sample()]
-        },
-        nextFunction = {
-          this[tm[keys.indexOf(it)]
-            .asDNArray().sample()]
-        })
-    }
+    transitionMatrix().let { it to it.cdfs() }
+      .let { (tm, cdfs) ->
+        generateSequence(
+          seedFunction = {
+            this[mk.math.sumD2(tm, 1)
+              .toList().cdf().sample()]
+          },
+          nextFunction = {
+            this[cdfs[keys.indexOf(it)].sample()]
+          })
+      }
 
   fun isErgodic() =
     transitionMatrix().let { it ->
@@ -70,16 +78,16 @@ class MarkovChain<T> {
 operator fun <T> Set<T>.times(s: Set<T>) =
   flatMap { l -> s.map { r -> l to r }.toSet() }.toSet()
 
-fun Ndarray<Double, *>.sample() =
-  asD1Array().toList().sample()
+fun Ndarray<Double, D2>.cdfs() =
+  (0 until shape[0]).map { this[it].toList().cdf() }
 
-fun List<Number>.sample(): Int =
+fun List<Number>.cdf(): CDF = CDF(
   map { it.toDouble() }.sum()
     .let { sum -> map { i -> i.toDouble() / sum } }
     .runningReduce { acc, d -> d + acc }
-    .let { cdf ->
-      val rand = Random.nextDouble()
-      cdf.indices.minByOrNull { i ->
-        (cdf[i] - rand).let { if(it < 0) 2.0 else it }
-      }!!
-    }
+)
+
+class CDF(val cdf: List<Double>): List<Double> by cdf
+
+fun CDF.sample(rand: Double = Random.nextDouble()) =
+  indices.first { i -> 0 < this[i] - rand }
