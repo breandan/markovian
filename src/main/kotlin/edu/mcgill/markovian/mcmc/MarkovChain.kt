9b55be057247bf1be74554cc192a6a9ca1e88570
breandan <bre@ndan.co>
2021-01-25 18:40:39 -1100

    refactor chain

--- src/main/kotlin/edu/mcgill/markovian/mcmc/MarkovChain.kt
+++ src/main/kotlin/edu/mcgill/markovian/mcmc/MarkovChain.kt
@@ -8,34 +8,52 @@ import java.io.File
 import kotlin.math.abs
 import kotlin.random.Random
 import kotlin.streams.asStream
-import kotlin.system.measureTimeMillis
+import kotlin.time.*
 
+@ExperimentalTime
 fun main(args: Array<String>) {
   println("Training data: ${args[0]}")
-  val data = File(args[0]).walkTopDown()
-    .filter { it.extension == "py" }
-    .joinToString { it.readText() }.asSequence()
+  val mc = measureTimedValue {
+    val data = File(args[0]).walkTopDown()
+      .filter { it.extension == "py" }
+      .joinToString { it.readText() }.asSequence()
 
-  val mc = data.toMarkovChain(memory = 3)
+    data.toMarkovChain(memory = 3)
+  }.let {
+    println("Training time: ${it.duration}")
+    it.value
+  }
+
   println("Tokens: " + mc.size)
-  measureTimeMillis {
-    val sample = mc.sample().take(200).flatten()
-    println("Sample: " + sample.joinToString(""))
-  }.also { println("Sampling time: $it ms") }
+//  measureTimedValue {
+//    println("Ergodic:" + mc.isErgodic())
+//  }.also { println("Ergodicity time: ${it.duration}") }
+
+  measureTimedValue {
+    mc.sample().take(200).flatten().toList()
+  }.also {
+    println("Sample: " + it.value.joinToString(""))
+    println("Sampling time: ${it.duration}")
+  }
 }
 
 fun <T> Sequence<T>.toMarkovChain(memory: Int = 1) =
-  MarkovChain<List<T>>().also { mc ->
-    (0 until memory)
-      .flatMap { drop(it).chunked(memory) }
-      .asSequence().asStream().parallel()
-      .reduce { prev, curr ->
-        mc.observe(prev to curr)
-        curr
+  MarkovChain((0 until memory)
+    .flatMap { s -> drop(s).chunked(memory) }
+    .asSequence())
+
+class MarkovChain<T>(
+  sequence: Sequence<T>,
+  val maxTokens: Int = 2000
+) {
+  val counts = AtomicLongMap.create<Pair<T, T>>()
+  init {
+    sequence.zipWithNext().asStream().parallel()
+      .forEach { (tp, tn) ->
+        counts.incrementAndGet(tp to tn)
       }
   }
 
-class MarkovChain<T>(val maxTokens: Int = 2000) {
   private val keys by lazy {
     counts.asMap().entries.asSequence()
       .sortedByDescending { it.value }
@@ -44,39 +62,33 @@ class MarkovChain<T>(val maxTokens: Int = 2000) {
       .flatten().distinct().toList()
   }
   val size by lazy { keys.size }
-  val counts = AtomicLongMap.create<Pair<T, T>>()
-
-  fun sample() =
-    transitionMatrix().let { it to it.cdfs() }
-      .let { (tm, cdfs) ->
-        generateSequence(
-          seedFunction = {
-            this[mk.math.sumD2(tm, 1)
-              .toList().cdf().sample()]
-          },
-          nextFunction = {
-            this[cdfs[keys.indexOf(it)].sample()]
-          }
-        )
-      }
-
-  fun isErgodic() = transitionMatrix().let { it ->
-    mk.linalg.pow(it + mk.identity(size), size)
-      .all { 0.0 < it }
-  }
-
-  private operator fun get(index: Int) = keys[index]
-
-  fun transitionMatrix() =
+  val tm by lazy { //Transition matrix
     mk.d2array(size, size) { 0.0 }.also { mt ->
       keys.indices.toSet().let { it * it }
         .forEach { (i, j) ->
           mt[i, j] = this[i, j].toDouble()
         }
     }.let { it / it.sum() }
+  }
+  val cdfs by lazy { // Computes row-wise CDFs
+    (0 until size).map { tm[it].toList().cdf() }
+  }
 
-  fun observe(pair: Pair<T, T>) =
-    counts.incrementAndGet(pair)
+  fun sample() = generateSequence(
+    seedFunction = {
+      this[mk.math.sumD2(tm, 1)
+        .toList().cdf().sample()]
+    },
+    nextFunction = {
+      this[cdfs[keys.indexOf(it)].sample()]
+    }
+  )
+
+  fun isErgodic() =
+    mk.linalg.pow(tm + mk.identity(size), size)
+      .all { 0.0 < it }
+
+  private operator fun get(index: Int) = keys[index]
 
   operator fun get(i: Int, j: Int) =
     counts[keys[i] to keys[j]]
@@ -86,9 +98,6 @@ class MarkovChain<T>(val maxTokens: Int = 2000) {
 operator fun <T> Set<T>.times(s: Set<T>) =
   flatMap { ti -> s.map { ti to it }.toSet() }.toSet()
 
-fun Ndarray<Double, D2>.cdfs() =
-  (0 until shape[0]).map { this[it].toList().cdf() }
-
 fun List<Number>.cdf(): CDF = CDF(
   map { it.toDouble() }.sum()
     .let { sum -> map { i -> i.toDouble() / sum } }
@@ -97,6 +106,7 @@ fun List<Number>.cdf(): CDF = CDF(
 
 class CDF(val cdf: List<Double>): List<Double> by cdf
 
+// Computes KS-transform using binary search
 fun CDF.sample(rand: Double = Random.nextDouble()) =
   cdf.binarySearch { it.compareTo(rand) }
     .let { if (it < 0) abs(it) - 1 else it }
