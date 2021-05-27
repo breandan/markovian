9b6a87836c066b3142e6bf2117ea1ccac5c87720
breandan <bre@ndan.co>
2021-05-27 18:47:20 -0400

    remove IMP

--- src/main/kotlin/edu/mcgill/markovian/mcmc/MarkovProcess.kt
+++ src/main/kotlin/edu/mcgill/markovian/mcmc/MarkovProcess.kt
@@ -1,46 +1,14 @@
 package edu.mcgill.markovian.mcmc
 
 import edu.mcgill.kaliningraph.*
-import edu.mcgill.kaliningraph.typefamily.IGF
-
-interface IMP: IGF<MarkovProcess, Transition, State> {
-  override fun Edge(s: State, t: State) = Transition(s, t)
-
-  override fun Graph(vertices: Set<State>) = MarkovProcess(vertices)
-
-  override fun Vertex(
-    newId: String,
-    edgeMap: (State) -> Set<Transition>
-  ) = State(newId, edgeMap)
-
-}
-
 
 class MarkovProcess(states: Set<State>):
-  IMP, Graph<MarkovProcess, Transition, State>(states) {
-}
+  Graph<MarkovProcess, Transition, State>(states)
 
 class Transition(source: State, target: State):
-  IMP, Edge<MarkovProcess, Transition, State>(source, target) {
-}
+  Edge<MarkovProcess, Transition, State>(source, target)
 
 class State(
   id: String,
-  override val edgeMap: (State) -> Collection<Transition>
-): IMP, Vertex<MarkovProcess, Transition, State>(id) {
-
-  override fun Edge(s: State, t: State): Transition {
-    TODO("Not yet implemented")
-  }
-
-  override fun Graph(vertices: Set<State>): MarkovProcess {
-    TODO("Not yet implemented")
-  }
-
-  override fun Vertex(
-    newId: String,
-    edgeMap: (State) -> Set<Transition>
-  ): State {
-    TODO("Not yet implemented")
-  }
-}
+  override val edgeMap: (State) -> Set<Transition>
+): Vertex<MarkovProcess, Transition, State>(id)
