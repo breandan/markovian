75555671af63b22e9ae3eff1b367073685e0786f
breandan.considine <breandan.considine@gmail.com>
2021-07-17 23:59:44 -0400

    use disintegration theorem and document

--- src/main/kotlin/edu/mcgill/markovian/mcmc/MarkovChain.kt
+++ src/main/kotlin/edu/mcgill/markovian/mcmc/MarkovChain.kt
@@ -1,38 +1,37 @@
 package edu.mcgill.markovian.mcmc
 
 import com.google.common.util.concurrent.AtomicLongMap
+import edu.mcgill.kaliningraph.circuits.b
 import edu.mcgill.markovian.concurrency.*
 import org.jetbrains.kotlinx.multik.api.*
 import org.jetbrains.kotlinx.multik.ndarray.data.*
 import org.jetbrains.kotlinx.multik.ndarray.operations.*
 import kotlin.math.abs
 import kotlin.random.Random
-import kotlin.time.*
+import kotlin.time.ExperimentalTime
 
+/**
+ * Marginalizes/sums out all dimensions not contained in [dims],
+ * producing a rank-(dims.size) tensor consisting of [dims].
+ */
 fun NDArray<Double, DN>.sumOnto(vararg dims: Int = intArrayOf(0)) =
   (0 until dim.d).fold(this to 0) { (t, r), b ->
     if (b in dims) t to r + 1
     else mk.math.sum<Double, DN, DN>(t, r) to r
   }.first
 
+/**
+ * Each entry in [dimToIdx] defines a unique N-1 dimensional hyperplane,
+ * which, when intersected, produce a rank-(N-dimToIdx.size) tensor.
+ */
 fun NDArray<Double, DN>.disintegrate(dimToIdx: Map<Int, Int>) =
+  // TODO: Is this really disintegration or something else?
+  // http://www.stat.yale.edu/~jtc5/papers/ConditioningAsDisintegration.pdf
+  // https://en.wikipedia.org/wiki/Disintegration_theorem
   (0 until dim.d).fold(this to 0) { (t, r), b ->
     if (b in dimToIdx) t.view(dimToIdx[b]!!, r).asDNArray() to r
     else t to r + 1
   }.first
-
-@ExperimentalTime
-fun main() {
-  val a = mk.ndarray(
-    mk[
-      mk[mk[1.0, 2.0, 3.0], mk[4.0, 5.0, 6.0], mk[1.0, 1.0, 1.0]],
-      mk[mk[7.0, 8.0, 9.0], mk[10.0, 11.0, 12.0], mk[2.0, 2.0, 2.0]],
-      mk[mk[13.0, 14.0, 15.0], mk[16.0, 17.0, 18.0], mk[3.0, 3.0, 3.0]]
-    ]
-  )
-  println(a.asDNArray().disintegrate(mapOf(1 to 1, 2 to 2)))
-  println(a.asDNArray().slice(mapOf(1 to Slice(1, 2, 1), 2 to Slice(2, 3, 1))))
-}
 
 fun <T> Sequence<T>.toMarkovChain(memory: Int = 3) =
   MarkovChain(train = this, memory = memory)
@@ -41,14 +40,16 @@ fun <T> Sequence<T>.toMarkovChain(memory: Int = 3) =
 class Bijection<T>(
   val list: List<T>,
   val map: Map<T, Int> = list.zip(list.indices).toMap(),
-  val revMap: Map<Int, T> = map.entries.associate { (k, v) -> v to k }
-): Map<T, Int> by map {
+  val rmap: Map<Int, T> = map.entries.associate { (k, v) -> v to k }
+) : Map<T, Int> by map {
   // 𝒪(1) surrogate for List<T>.indexOf(...)
-  operator fun get(key: Int): T = revMap[key]!!
+  operator fun get(key: Int): T = rmap[key]!!
   override fun get(key: T): Int = map[key]!!
-  operator fun contains(value: Int) = value in revMap
+  operator fun contains(value: Int) = value in rmap
 }
 
+// TODO: Support continuous state spaces?
+// https://www.colorado.edu/amath/sites/default/files/attached-files/2_28_2018.pdf
 open class MarkovChain<T>(
   val maxTokens: Int = 2000,
   train: Sequence<T> = sequenceOf(),
@@ -73,9 +74,9 @@ open class MarkovChain<T>(
    *
    * P(T₁=t₁,T₂=t₂,…,Tₙ=tₙ) = P(tₙ|tₙ₋₁)P(tₙ₋₁|tₙ₋₂)…P(t₂|t₁)
    *
-   * Where n is the tensor rank, T₁...ₙ are random variables and
-   * t₁...ₙ are their concrete instantiations. This tensor is a
-   * hypercube with shape [size]ⁿ, indexed by [dictionary].
+   * Where the tensor rank n=[memory], T₁...ₙ are random variables
+   * and t₁...ₙ are their concrete instantiations. This tensor is
+   * a hypercube with shape [size]ⁿ, indexed by [dictionary].
    */
 
   val tt: NDArray<Double, DN> by resettableLazy(mgr) {
@@ -87,17 +88,20 @@ open class MarkovChain<T>(
             val idx = k.map { dictionary[it] }.toIntArray()
             if (idx.size == memory) mt[idx] = v.toDouble()
           }
-      }.let { it / it.sum() }
+      }.let { it / it.sum() } // Normalize across all entries in tensor
+    // TODO: May be possible to precompute fiber/slice PMFs via tensor renormalization?
+    // https://mathoverflow.net/questions/393427/generalization-of-sinkhorn-s-theorem-to-stochastic-tensors
+    // https://arxiv.org/pdf/1702.08142.pdf
+    // TODO: Look into copulae
+    // https://en.wikipedia.org/wiki/Copula_(probability_theory)
   }
 
-  // Computes row-wise CDFs
-  val cdfs: MutableMap<List<Int>, CDF> by resettableLazy(mgr) { mutableMapOf() }
-  // TODO: Preconditioning? https://mathoverflow.net/questions/393427/generalization-of-sinkhorn-s-theorem-to-stochastic-tensors
-
-  fun <T> AtomicLongMap<T>.addAll(that: AtomicLongMap<T>) =
-    that.asMap().forEach { (k, v) -> addAndGet(k, v) }
+  // Maps the coordinates of a transition tensor fiber to a memoized distribution
+  val dists: MutableMap<List<Int>, Dist> by resettableLazy(mgr) { mutableMapOf() }
 
   operator fun plus(mc: MarkovChain<T>) = apply {
+    fun <T> AtomicLongMap<T>.addAll(that: AtomicLongMap<T>) =
+      that.asMap().forEach { (k, v) -> addAndGet(k, v) }
     counter.rawCounts.addAll(mc.counter.rawCounts)
     counter.memCounts.addAll(mc.counter.memCounts)
     mgr.reset()
@@ -105,39 +109,32 @@ open class MarkovChain<T>(
 
   fun sample(
     seed: () -> T = {
-      dictionary[tt.sumOnto().toList().cdf().sample()]
+      dictionary[Dist(tt.sumOnto().toList()).sample()]
     },
     next: (T) -> T = { it: T ->
-      val cdf = cdfs.getOrPut(listOf(dictionary[it])) {
-        tt.view(dictionary[it]).asDNArray().sumOnto().toList().cdf()
-      }
-      dictionary[cdf.sample()]
+      val dist = Dist(tt.view(dictionary[it]).asDNArray().sumOnto().toList())
+      dictionary[dist.sample()]
     },
     memSeed: () -> Sequence<T> = {
       generateSequence(seed, next).take(memory - 1)
     },
-    memNext: (Sequence<T>) -> (Sequence<T>) = {
-      val idxs = it.map { dictionary[it] }.toList()
-      val cdf = cdfs.getOrPut(idxs) {
-        // seems to work? I wonder why we don't need
-        // to use multiplication to express conditional
-        // probability? Just disintegration?
-        // https://blog.wtf.sg/posts/2021-03-14-smoothing-with-backprop/
+    memNext: (Sequence<T>) -> (Sequence<T>) = { curr ->
+      val idxs = curr.map { dictionary[it] }.toList()
+      val dist = dists.getOrPut(idxs) {
+        // seems to work? I wonder why we don't need to use multiplication
+        // to express conditional probability? Just disintegration?
+        // https://blog.wtf.sg/posts/2021-03-14-smoothing-with-backprop
         // https://homes.sice.indiana.edu/ccshan/rational/disintegrator.pdf
-        val slices = idxs.map { Slice(it, it + 1, 1) }
-        val volume = slices.indices.zip(slices).toMap()
-        tt.slice(volume).toList().cdf()
+        val slices = idxs.indices.zip(idxs).toMap()
+        // Intersect conditional slices to produce a 1D count fiber
+        val intersection = tt.disintegrate(slices).toList()
+        // Turns 1D count fiber into a probability vector
+        Dist(intersection)
       }
 
-      it.drop(1) + dictionary[cdf.sample()]
+      curr.drop(1) + dictionary[dist.sample()]
     }
   ) = generateSequence(memSeed, memNext).map { it.last() }
-
-//  fun isErgodic(): Boolean =
-//    mk.linalg.pow(tt, (size - 1) * (size - 1) + 1)
-//      .all { 0.0 < it }
-
-  operator fun get(i: IntArray) = counter[i.map { dictionary[it] }] ?: 0
 
   /**
    * Treats each subsequence of length [memory] as a single
@@ -146,7 +143,9 @@ open class MarkovChain<T>(
   class Counter<T>(
     count: Sequence<T> = sequenceOf(),
     memory: Int,
+    // Counts raw instances of T
     val rawCounts: AtomicLongMap<T> = AtomicLongMap.create(),
+    // Counts unique subsequences of Ts up length memory
     val memCounts: AtomicLongMap<List<T>> =
       AtomicLongMap.create<List<T>>().also { memMap ->
         (0 until memory)
@@ -156,20 +155,21 @@ open class MarkovChain<T>(
             buffer.forEach { rawCounts.incrementAndGet(it) }
           }
       }
-  ): Map<List<T>, Long> by memCounts.asMap()
+  ) : Map<List<T>, Long> by memCounts.asMap()
 }
 
-fun Collection<Number>.cdf() = CDF(
-  sumOf { it.toDouble() }
-    .let { sum -> map { i -> i.toDouble() / sum } }
-    .runningReduce { acc, d -> d + acc }
-)
-
-class CDF(val cdf: List<Double>): List<Double> by cdf
-
-// Computes KS-transform using binary search
-fun CDF.sample(
-  random: Random = Random.Default,
-  target: Double = random.nextDouble()
-): Int = cdf.binarySearch { it.compareTo(target) }
-  .let { if (it < 0) abs(it) - 1 else it }
+class Dist(
+  counts: Collection<Number>,
+  val sum: Double = counts.sumOf { it.toDouble() },
+  // https://en.wikipedia.org/wiki/Probability_mass_function
+  val pmf: List<Double> = counts.map { i -> i.toDouble() / sum },
+  // https://en.wikipedia.org/wiki/Cumulative_distribution_function
+  val cdf: List<Double> = pmf.runningReduce { acc, d -> d + acc }
+) {
+  // Computes KS-transform using binary search
+  fun sample(
+    random: Random = Random.Default,
+    target: Double = random.nextDouble()
+  ): Int = cdf.binarySearch { it.compareTo(target) }
+    .let { if (it < 0) abs(it) - 1 else it }
+}
--- src/test/kotlin/edu/mcgill/markovian/mcmc/MarkovChainTest.kt
+++ src/test/kotlin/edu/mcgill/markovian/mcmc/MarkovChainTest.kt
@@ -1,6 +1,9 @@
 package edu.mcgill.markovian.mcmc
 
-import org.junit.Test
+import org.jetbrains.kotlinx.multik.api.*
+import org.jetbrains.kotlinx.multik.ndarray.data.*
+import org.jetbrains.kotlinx.multik.ndarray.operations.toList
+import org.junit.*
 import kotlin.random.Random
 
 class MarkovChainTest {
@@ -17,5 +20,33 @@ class MarkovChainTest {
     List(100) { Random.nextDouble() }.asSequence()
       .toMarkovChain().sample().take(100)
       .let { println(it.joinToString(",")) }
+  }
+
+  @Test
+  fun testDisintegrationMakesSense() {
+    val a = mk.ndarray(
+      mk[
+        mk[mk[1.0, 2.0, 3.0], mk[4.0, 5.0, 6.0], mk[1.0, 1.0, 1.0]],
+        mk[mk[7.0, 8.0, 9.0], mk[10.0, 11.0, 12.0], mk[2.0, 2.0, 2.0]],
+        mk[mk[13.0, 14.0, 15.0], mk[16.0, 17.0, 18.0], mk[3.0, 3.0, 3.0]]
+      ]
+    )
+
+    Assert.assertEquals(
+      a.asDNArray().disintegrate(
+        mapOf(
+        //D    I
+          1 to 1,
+          2 to 2
+        )
+      ),
+      a.asDNArray().slice(
+        mapOf(
+        //D                  I         I+1
+          1 to Slice(start = 1, stop = 2, step = 1),
+          2 to Slice(start = 2, stop = 3, step = 1)
+        )
+      ).toList()
+    )
   }
 }
